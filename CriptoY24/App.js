246d61afb4f3b59f708eb8e119ebbfa882d79fec
Manuel Corales <manuelcorales0712@gmail.com>
2020-12-28 21:23:37 -0300

    App terminada

--- CriptoY24/App.js
+++ CriptoY24/App.js
@@ -16,6 +16,7 @@ import {
 	Text,
 	StatusBar,
 	TextInput,
+	Switch,
 	Button,
 	Alert,
 	Vibration
@@ -32,8 +33,11 @@ class Aplicacion extends Component {
 		super();
 		this.state = {
 			arrayNumeros: [],
-			es24: true,
+			numeroAObtener: 24,
+			mostrarSolucion: false,
 		}
+		this.mostrarAlerta = this.mostrarAlerta.bind(this);
+		this.actualizarNumeros = this.actualizarNumeros.bind(this);
 	}
 
 	obtenerColorRandom() {
@@ -45,13 +49,187 @@ class Aplicacion extends Component {
 		return color;
 	}
 
+	actualizarNumeros(index, numero){
+		let arrayNumeros = this.state.arrayNumeros;
+		arrayNumeros[index] = parseInt(numero);
+		this.setState({
+			arrayNumeros
+		})
+	}
+
 	mostrarAlerta(){
+		let resultado = 0;
+		let array4Numeros = this.state.arrayNumeros;
+		let contador = 0;
+		let soluciones = [];
+		let solucion = "";
+		let solucionParcial = "";
+		for(let numero1 of array4Numeros){
+			let array3Numeros = Array.from(array4Numeros);
+			array3Numeros.splice(array4Numeros.findIndex(x => x == numero1), 1);
+			for(let numero2 of array3Numeros){
+				let array2Numeros = Array.from(array3Numeros);
+				array2Numeros.splice(array3Numeros.findIndex(x => x == numero2), 1);
+				for(let numero3 of array2Numeros){
+					let array1Numeros = Array.from(array2Numeros);
+					array1Numeros.splice(array2Numeros.findIndex(x => x == numero3), 1);
+					for(let numero4 of array1Numeros){
+						contador++;
+						for(let agrupacion = 0; agrupacion < 3; agrupacion++){
+							for(let operador1 = 0; operador1 < 4; operador1++){
+								for(let operador2 = 0; operador2 < 4; operador2++){
+									for(let operador3 = 0; operador3 < 4; operador3++){										
+										if(agrupacion == 0){
+											if(operador1 == 0){
+												resultado = numero1 + numero2;
+												solucion = solucion.concat(`${numero1} + ${numero2}`);
+											} else if(operador1 == 1) {
+												resultado = numero1 - numero2;
+												solucion = solucion.concat(`${numero1} - ${numero2}`);
+											} else if(operador1 == 2) {
+												resultado = numero1 * numero2;
+												solucion = solucion.concat(`${numero1} * ${numero2}`);
+											} else if(operador1 == 3) {
+												resultado = numero1 / numero2;
+												solucion = solucion.concat(`${numero1} / ${numero2}`);
+											}
+											if(operador2 == 0){
+												resultado += numero3;
+												solucion = solucion.concat(` + ${numero3}`);
+											} else if(operador2 == 1) {
+												resultado -= numero3;
+												solucion = solucion.concat(` - ${numero3}`);
+											} else if(operador2 == 2) {
+												resultado = resultado * numero3;
+												solucion = solucion.concat(` * ${numero3}`);
+											} else if(operador2 == 3) {
+												resultado = resultado / numero3;
+												solucion = solucion.concat(` / ${numero3}`);
+											}
+											if(operador3 == 0){
+												resultado += numero4;
+												solucion = solucion.concat(` + ${numero4}`);
+											} else if(operador3 == 1) {
+												resultado -= numero4;
+												solucion = solucion.concat(` - ${numero4}`);
+											} else if(operador3 == 2) {
+												resultado = resultado * numero4;
+												solucion = solucion.concat(` * ${numero4}`);
+											} else if(operador3 == 3) {
+												resultado = resultado / numero4;
+												solucion = solucion.concat(` / ${numero4}`);
+											}
+										} else if (agrupacion == 1) {
+											let resultadoParcial;
+											if(operador1 == 0){
+												resultado = numero1 + numero2;
+												solucion = solucion.concat(`${numero1} + ${numero2}`);
+											} else if(operador1 == 1) {
+												resultado = numero1 - numero2;
+												solucion = solucion.concat(`${numero1} - ${numero2}`);
+											} else if(operador1 == 2) {
+												resultado = numero1 * numero2;
+												solucion = solucion.concat(`${numero1} * ${numero2}`);
+											} else if(operador1 == 3) {
+												resultado = numero1 / numero2;
+												solucion = solucion.concat(`${numero1} / ${numero2}`);
+											}
+											if(operador3 == 0){
+												resultadoParcial = numero3 + numero4;
+												solucionParcial = solucionParcial.concat(`${numero3} + ${numero4}`);
+											} else if(operador3 == 1) {
+												resultadoParcial = numero3 - numero4;
+												solucionParcial = solucionParcial.concat(`${numero3} - ${numero4}`);
+											} else if(operador3 == 2) {
+												resultadoParcial = numero3 * numero4;
+												solucionParcial = solucionParcial.concat(`${numero3} * ${numero4}`);
+											} else if(operador3 == 3) {
+												resultadoParcial = numero3 / numero4;
+												solucionParcial = solucionParcial.concat(`${numero3} / ${numero4}`);
+											}
+											if(operador2 == 0){
+												resultado = resultado + resultadoParcial;
+												solucion = solucion.concat(` + (${solucionParcial})`);
+											} else if(operador2 == 1) {
+												resultado = resultado - resultadoParcial;
+												solucion = solucion.concat(` - (${solucionParcial})`);
+											} else if(operador2 == 2) {
+												resultado = resultado * resultadoParcial;
+												solucion = solucion.concat(` * (${solucionParcial})`);
+											} else if(operador2 == 3) {
+												resultado = resultado / resultadoParcial;
+												solucion = solucion.concat(` / (${solucionParcial})`);
+											}
+											
+										} else if (agrupacion == 2) {
+											let resultadoParcial;
+											if(operador2 == 0){
+												resultadoParcial = numero2 + numero3;
+												solucionParcial = solucionParcial.concat(`(${numero2} + ${numero3})`);
+											} else if(operador2 == 1) {
+												resultadoParcial = numero2 - numero3;
+												solucionParcial = solucionParcial.concat(`(${numero2} - ${numero3})`);
+											} else if(operador2 == 2) {
+												resultadoParcial = numero2 * numero3;
+												solucionParcial = solucionParcial.concat(`(${numero2} * ${numero3})`);
+											} else if(operador2 == 3) {
+												resultadoParcial = numero2 / numero3;
+												solucionParcial = solucionParcial.concat(`(${numero2} / ${numero3})`);
+											}
+											if(operador1 == 0){
+												resultado = numero1 + resultadoParcial;
+												solucion = solucion.concat(`${numero1} + ${solucionParcial}`);
+											} else if(operador1 == 1) {
+												resultado = numero1 - resultadoParcial;
+												solucion = solucion.concat(`${numero1} - ${solucionParcial}`);
+											} else if(operador1 == 2) {
+												resultado = numero1 * resultadoParcial;
+												solucion = solucion.concat(`${numero1} * ${solucionParcial}`);
+											} else if(operador1 == 3) {
+												resultado = numero1 / resultadoParcial;
+												solucion = solucion.concat(`${numero1} / ${solucionParcial}`);
+											}
+											if(operador3 == 0){
+												resultado += numero4;
+												solucion = solucion.concat(` + ${numero4}`);
+											} else if(operador3 == 1) {
+												resultado -= numero4;
+												solucion = solucion.concat(` - ${numero4}`);
+											} else if(operador3 == 2) {
+												resultado = resultado * numero4;
+												solucion = solucion.concat(` * ${numero4}`);
+											} else if(operador3 == 3) {
+												resultado = resultado / numero4;
+												solucion = solucion.concat(` / ${numero4}`);
+											}	
+										}
+										if(resultado == this.state.numeroAObtener && !soluciones.find(sol => sol == solucion)){
+											soluciones.push(solucion);
+										}
+										solucion = "";
+										solucionParcial = "";
+									}
+								}	
+							}
+						}
+					}
+				}
+			}
+		}
 		Vibration.vibrate(150, false);
+		let tituloAlerta;
+		if(soluciones.length == 0) {
+			tituloAlerta = `No sale`;
+		} else if (soluciones.length == 1) {
+			tituloAlerta = "Sale de 1 forma";
+		} else {
+			tituloAlerta = `Sale de varias formas (${soluciones.length})`;
+		}
 		Alert.alert(
-			'Sale de varias formas',
-			null,
+			tituloAlerta,
+			this.state.mostrarSolucion ? soluciones.join("\r\n"): null,
 			[
-			  { text: 'Oki', onPress: () => console.log('OK Pressed') }
+			  { text: 'Oki' },
 			],
 			{ cancelable: true }
 		  );
@@ -64,7 +242,7 @@ class Aplicacion extends Component {
 			<Header style={styles.header}>
 				<Text style={styles.headerTexto}>
 					{
-						this.state.es24 == true ?
+						this.state.numeroAObtener == 24 ?
 						"24":
 						"Cripto"
 					}
@@ -74,28 +252,41 @@ class Aplicacion extends Component {
 				<ScrollView
 				contentInsetAdjustmentBehavior="automatic"
 				style={styles.scrollView}>
-
-				<View style={styles.body}>
-					<View style={styles.sectionContainer}>
-						<View style={styles.textInputContainerNumeros}>
-							<Item style={{width: 60}} regular>
-								<Input styles={styles.textInputNumeros} keyboardType={"numeric"} maxLength={1}></Input>
-							</Item>
-							<Item style={{width: 60}} regular>
-								<Input styles={styles.textInputNumeros} keyboardType={"numeric"} maxLength={1}></Input>
-							</Item>
-							<Item style={{width: 60}} regular>
-								<Input styles={styles.textInputNumeros} keyboardType={"numeric"} maxLength={1}></Input>
-							</Item>
-							<Item style={{width: 60}} regular>
-								<Input styles={styles.textInputNumeros} keyboardType={"numeric"} maxLength={1}></Input>
-							</Item>
+					<View style={styles.body}>
+						<View style={styles.sectionContainer}>
+							<View style={styles.textInputContainerNumeros}>
+								<Item style={{width: 60}} regular>
+									<Input styles={styles.textInputNumeros} keyboardType={"numeric"} maxLength={2} onChangeText={(numero) => this.actualizarNumeros(0, numero)}></Input>
+								</Item>
+								<Item style={{width: 60}} regular>
+									<Input styles={styles.textInputNumeros} keyboardType={"numeric"} maxLength={2} onChangeText={(numero) => this.actualizarNumeros(1, numero)}></Input>
+								</Item>
+								<Item style={{width: 60}} regular>
+									<Input styles={styles.textInputNumeros} keyboardType={"numeric"} maxLength={2} onChangeText={(numero) => this.actualizarNumeros(2, numero)}></Input>
+								</Item>
+								<Item style={{width: 60}} regular>
+									<Input styles={styles.textInputNumeros} keyboardType={"numeric"} maxLength={2} onChangeText={(numero) => this.actualizarNumeros(3, numero)}></Input>
+								</Item>
+								{
+									this.state.numeroAObtener != 24 &&
+									<Item style={{width: 60}} regular>
+										<Input styles={styles.textInputNumeros} keyboardType={"numeric"} maxLength={2} onChangeText={(numero) => this.setState({numeroAObtener: parseInt(numero)})}></Input>
+									</Item>
+								}
+							</View>
+							<Button title={"Ver posibilidades"} color={this.obtenerColorRandom()} onPress={this.mostrarAlerta} style={styles.posibilidadesBoton}/>
+							<Button title={this.state.numeroAObtener == 24 ? "Cambiar a Cripto" : "Cambiar a 24"} color={this.obtenerColorRandom()} onPress={() => this.setState({numeroAObtener: this.state.numeroAObtener == 24 ? null: 24})} style={styles.alternarJuegoBoton}/>
+							<Text style={styles.mostrarSolucion}>
+								Mostrar solucion
+							</Text>
+								<Switch
+									trackColor={{ false: "#767577", true: this.obtenerColorRandom() }}
+									thumbColor={this.state.mostrarSolucion ? this.obtenerColorRandom() : "#767577"}
+									onValueChange={() => this.setState({mostrarSolucion: !this.state.mostrarSolucion})}
+									value={this.state.mostrarSolucion}
+								/>
 						</View>
-						<Button title={"Ver posibilidades"} color={this.obtenerColorRandom()} onPress={this.mostrarAlerta} style={styles.posibilidadesBoton}/>
-						
-						<Button title={"Cambiar a Cripto <Por hacerse>"} color={this.obtenerColorRandom()} style={styles.alternarJuegoBoton}/>
 					</View>
-				</View>
 				</ScrollView>
 			</SafeAreaView>
 			</>
@@ -109,6 +300,9 @@ const styles = StyleSheet.create({
 	},
 	posibilidadesBoton: {
 		marginBottom: 10
+	},
+	mostrarSolucion: {
+		marginTop: 30
 	},
 	alternarJuegoBoton: {
 		margin: 20
